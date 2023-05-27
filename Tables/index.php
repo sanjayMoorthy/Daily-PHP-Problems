f7079a2dc8ec9d663611f4744b583de6826f1741
sanjayMoorthy <sanjaysdckap@gmail.com>
2023-05-27 11:54:08 +0530

    Small changes

--- Tables/index.php
+++ Tables/index.php
@@ -1,6 +1,7 @@
 <?php
 
-$input = readLine("please enter your input");
-for($i=1; $i<=10;$i++){
+$input = readLine("please enter which table you want EX: 4th table ");
+$tableLength = readLine("please enter your table length Ex: 10*10 or 20*20 ");
+for($i=1; $i<=$tableLength;$i++){
     echo $i." * ".$input." = ".$i*$input."\n";
 }
