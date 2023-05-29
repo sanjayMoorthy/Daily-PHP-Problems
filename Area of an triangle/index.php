70f40802f70f82e0927b2c088d026c57d5142038
sanjayMoorthy <sanjaysdckap@gmail.com>
2023-05-29 16:55:57 +0530

    task completed

---
+++ Area of an triangle/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$base = ReadLine("Enter Base of triangle ");
+$height = ReadLine("Enter height of triangle ");
+
+
+$area = 1/2  * ($base * $height);
+echo "The area of traingle is $area cm²";
