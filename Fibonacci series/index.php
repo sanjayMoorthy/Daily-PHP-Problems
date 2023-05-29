024297aa040ba06b47dedb4410072e0416734f42
sanjayMoorthy <sanjaysdckap@gmail.com>
2023-05-29 12:55:20 +0530

    problem completed

---
+++ Fibonacci series/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$userInput = ReadLine("enter your input");
+
+$firstInput = 0;
+$secInput = 1;
+echo $firstInput."\n";
+echo $secInput."\n";
+$finaleValue = $firstInput + $secInput ;
+
+for($i=0; $i<$userInput;$i++){
+    // echo $userInput." ".$i."\n" ; 
+    echo $finaleValue. "\n";
+
+    $firstInput = $secInput;
+    $secInput = $finaleValue;
+    $finaleValue = $firstInput + $secInput;
+
+    // echo $finaleValue. "\n";
+    
+}
