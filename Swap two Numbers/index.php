993ac32a6ba3def5b7dc267aa1c3eb36854ee268
sanjayMoorthy <sanjaysdckap@gmail.com>
2023-06-01 16:52:47 +0530

    completed

---
+++ Swap two Numbers/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$firstInput = 77;
+$secondInput = 71;
+
+$firstInput = $firstInput + $secondInput;
+$secondInput = $firstInput - $secondInput;
+$firstInput = $firstInput - $secondInput;
+
+echo "First input = ".$firstInput."\n"."Second input = ".$secondInput;
+
+
+
+
